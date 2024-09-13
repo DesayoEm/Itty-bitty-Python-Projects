8fae2dff980f087128376a3658961493e8ffd2f9
Adesayo M <105006406+Adesayo20@users.noreply.github.com>
2024-09-13 13:46:21 +0100

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -1,25 +1,22 @@
 import random
-answer = random.randint(1,10)
+highest=10
+answer = random.randint(1,highest)
 while True:
     try:
-        guess = int(input("Enter a number between 1 and 10"))
-        if guess <1 or guess>10:
-            print("Your guess must be between 1 and 10")
+        guess = int(input(f"Enter a number between 1 and {highest}"))
+        if guess <1 or guess>highest:
+            print(f"Your guess must be between 1 and {highest}")
             continue
         break
     except ValueError:
         print("Invalid input. Please enter a valid number between 1 and 10.")
 
 
-if guess != answer:
+while guess != answer:
     if guess < answer:
         print("Please guess higher")
     else:
         print("Please guess lower")
     guess = int(input())
-    if guess == answer:
-        print(f"Well done! You guessed it. The correct number is {answer}")
-    else:
-        print(f"Sorry, you have not guessed correctly, the correct number is {answer}")
-else:
-    print("Nailed it at first trial!")
+
+print(f"Well done! You guessed it. The correct number is {answer}")
